86f070467320b239cebfc70c029fcc6531cb8436
Pasindu Yapa <lolmixage03@gmail.com>
2024-07-11 20:32:36 +0530

    DAO Types

---
+++ src/main/java/org/example/sms/types/DAOTypes.java
@@ -0,0 +1,15 @@
+package org.example.sms.types;
+
+public enum DAOTypes{
+    CUSTOMER,
+    EMPLOYEE,
+    SUPPLIER,
+    ITEM,
+    SUPPLIER_ORDER,
+    SUPPLIER_ORDER_DETAILS,
+    HAVEPAYINVOICE,
+    INVOICE,
+    ORDER,
+    DELIVERY,
+    PLACE_ORDER
+}
