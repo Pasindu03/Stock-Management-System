a0e48b24aaa57cde2bfc961e8b12c5b8931cbac0
Pasindu Yapa <lolmixage03@gmail.com>
2024-07-11 20:45:16 +0530

    CrudUtil Finished

--- src/main/java/org/example/sms/util/CrudUtil.java
+++ src/main/java/org/example/sms/util/CrudUtil.java
@@ -6,16 +6,15 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CrudUtil {
-    public static <T> T execute(String sql, Object...params) throws SQLException, ClassNotFoundException {
-        PreparedStatement statement =
-                DbConnection.getInstance().getConnection().prepareStatement(sql);
-        for (int i = 0; i < params.length; i++) {
-            statement.setObject((i+1),params[i]);
+    public static <T>T execute(String sql,Object... args) throws SQLException {
+        PreparedStatement preparedStatement = DbConnection.getInstance().getConnection().prepareStatement(sql);
+
+        for (int i = 0; i < args.length; i++) {
+            preparedStatement.setObject((i+1), args[i]);
         }
-        if (sql.startsWith("SELECT")||sql.startsWith("select")){
-            return (T) statement.executeQuery();
-        }else{
-            return ((T)(Boolean)(statement.executeUpdate()>0));
+        if (sql.startsWith("SELECT") || sql.startsWith("select")){
+            return (T) preparedStatement.executeQuery(); // resultSet
         }
+        return (T) (Boolean) (preparedStatement.executeUpdate() > 0);// boolean
     }
 }
